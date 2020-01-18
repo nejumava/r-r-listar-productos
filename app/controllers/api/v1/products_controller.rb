e0989152a457c22ece8e5dc0d1fc32c36527c302
Nelly Mahecha <nejumava@gmail.com>
2020-01-17 21:38:49 -0500

    Solución a reto - Crear producto

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::ProductsController < ApplicationController
         @products = Product.all
         render json: @products
     end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            render json: @product
+        else
+            render json: { errors: @product.errors }, status: 422
+        end
+    end
+
+    private
+    def product_params
+        params.require(:product).permit(:name, :price)
+    end
 end
