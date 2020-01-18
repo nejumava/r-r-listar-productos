0b260f63653f182e556200b4bb7cf8d947484fce
Nelly Mahecha <nejumava@gmail.com>
2020-01-17 19:07:32 -0500

    Solucion a reto

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::ProductsController < ApplicationController
+class Api::V1::ProductsController < ApplicationController
     def index
         @products = Product.all
         render json: @products
