ca700460eddb638c064aa656ec12ad45afe69b5e
Nelly Mahecha <nejumava@gmail.com>
2020-01-17 21:25:07 -0500

    Solucion a reto

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
     def index
         @products = Product.all
-        respond_to do |format|
-            format.json { render :json => @products }
-        end
+        render json: @products
     end
 end
